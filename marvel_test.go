b5c11ceb320b5bc7ef65a97f906440974088777b
Jason Hall <imjasonh@gmail.com>
2014-04-25 23:13:47 -0400

    move to marvel package, make test class a test

--- marvel_test.go
+++ marvel_test.go
@@ -1,14 +1,14 @@
-package main
+package marvel
 
 import (
-	"fmt"
+	"testing"
 )
 
-func main() {
+func TestRequest(t *testing.T) {
 	r, err := NewClient("d96b5157cfc7a60cbfaa715dc23c3eb1", "ccbc72b222419e2a4e40b4027f3bcb356142651b").Series(2258, CommonRequest{})
 	if err != nil {
-		fmt.Printf("err: %v\n", err)
+		t.Errorf("error: %v", err)
 		return
 	}
-	fmt.Printf("%+v", r)
+	t.Logf("%+v", r)
 }
