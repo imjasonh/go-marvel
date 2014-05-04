a11f1147c6936eaef4102a4b964aefd19058f44b
Jason Hall <imjasonh@gmail.com>
2014-05-04 13:42:28 -0400

    Add test case for getting single entity

--- marvel_test.go
+++ marvel_test.go
@@ -13,17 +13,23 @@ var (
 func TestRequest(t *testing.T) {
 	flag.Parse()
 
-	r, err := Client{
+	c := Client{
 		PublicKey:  *apiKey,
 		PrivateKey: *secret,
-	}.SingleSeries(2258).Comics(ComicsParams{})
+	}
+	r, err := c.SingleSeries(2258).Comics(ComicsParams{})
 	if err != nil {
 		t.Errorf("error: %v", err)
 		return
 	}
 	t.Logf("%+v", r.Data)
 	for _, iss := range r.Data.Results {
-		t.Logf(iss.Modified.Parse().String())
+		t.Logf("%v %s", *iss.IssueNumber, iss.Modified.Parse().String())
 		t.Logf(iss.Thumbnail.URL(PortraitIncredible))
 	}
+	comic, err := r.Data.Results[0].Get(c)
+	if err != nil {
+		t.Errorf("error getting: %v", err)
+	}
+	t.Logf("%+v", comic.Data.Results[0])
 }
